bf7aa39fe46791d1d647b8c1d5fb2db78a6ae0a3
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-12-23 04:59:00 +0200

    First commit

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+import { sequelize } from './shared/sequelize';
+
+import { FeedRouter } from './routes/feed.router';
+
+import bodyParser from 'body-parser';
+import { config } from './shared/config/config';
+import { FeedItem } from './models/FeedItem';
+
+
+(async () => {
+    await sequelize.addModels([FeedItem]);
+    await sequelize.sync();
+
+    const app = express();
+    const port = process.env.PORT || 8080;
+
+    app.use(bodyParser.json());
+
+    app.use(cors({
+        allowedHeaders: [
+            'Origin', 'X-Requested-With',
+            'Content-Type', 'Accept',
+            'X-Access-Token', 'Authorization',
+        ],
+        methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+        origin: config.url,
+    }));
+
+    app.get('/health', async (req: Request, res: Response) => {
+        // Health check
+        res.status(200).send('All clear')
+    });
+
+    app.use('/', FeedRouter);
+
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running ${config.url}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+})();
